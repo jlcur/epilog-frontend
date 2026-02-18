1ca700162ae9399fc7f46d56a42b7ae6106466df
jlcur <jordan@jordancurley.com>
2026-02-18 13:44:19 +0000

    Add head and app shell to root route

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -2,6 +2,7 @@ import type { QueryClient } from "@tanstack/react-query";
 import { createRootRouteWithContext, Outlet } from "@tanstack/react-router";
 import { TanStackRouterDevtools } from "@tanstack/react-router-devtools";
 import * as React from "react";
+import { Head } from "@/components/head/head";
 
 interface RouterContext {
 	queryClient: QueryClient;
@@ -15,7 +16,12 @@ export const Route = createRootRouteWithContext<RouterContext>()({
 function RootComponent() {
 	return (
 		<React.Fragment>
-			<div>Hello "__root"!</div>
+			<Head description="Epilog" />
+
+			<header>
+				<h1>Epilog</h1>
+			</header>
+
 			<main>
 				<Outlet />
 				{import.meta.env.DEV && <TanStackRouterDevtools />}
