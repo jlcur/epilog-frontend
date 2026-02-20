445c4ecfff4c468f2a95d697d4c2dc454672ae08
jlcur <jordan@jordancurley.com>
2026-02-20 15:19:35 +0000

    Add interceptor to API client

--- src/lib/api-client.ts
+++ src/lib/api-client.ts
@@ -4,3 +4,14 @@ import { env } from "@/config/env";
 export const api = Axios.create({
 	baseURL: env.API_URL,
 });
+
+/**
+ * Intercept the response so that instead of doing:
+ * const { data } = await api.get(endpoints.comments.all);
+ *
+ * We can do this:
+ * const data = await api.get(endpoints.comments.all);
+ */
+api.interceptors.response.use((response) => {
+	return response.data;
+});
