d7ad68df337842397e1b85d2fa25f26ad5ad0ea0
jlcur <jordan@jordancurley.com>
2026-02-21 17:10:41 +0000

    Add temporary theme switcher to header for easier use

--- src/components/layouts/header/header.tsx
+++ src/components/layouts/header/header.tsx
@@ -1,9 +1,13 @@
+import { ThemeSwitcher } from "@/components/ui/theme-switcher/theme-switcher";
 import styles from "./header.module.css";
 
 export const Header = () => {
 	return (
 		<header className={styles.container}>
 			<h1>Epilog</h1>
+			<div>
+				<ThemeSwitcher />
+			</div>
 		</header>
 	);
 };
