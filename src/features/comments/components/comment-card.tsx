fbabf441c110b31dd28b1550230020e1883218d7
jlcur <jordan@jordancurley.com>
2026-02-21 11:03:57 +0000

    Add functionality for copying link to comment to clipboard

--- src/features/comments/components/comment-card.tsx
+++ src/features/comments/components/comment-card.tsx
@@ -1,4 +1,5 @@
-import { Link } from "lucide-react";
+import { CopyButton } from "@/components/ui/copy-button/copy-button";
+import { paths } from "@/config/paths";
 import type { Comment } from "@/types/api";
 import { convertDateToDistance } from "@/utils/date-utils/format-date";
 import styles from "./comment-card.module.css";
@@ -9,13 +10,15 @@ type CommentCardProps = {
 
 export const CommentCard = ({ comment }: CommentCardProps) => {
 	const timeSinceCommentPosted = convertDateToDistance(comment.created_at);
+	// TODO: replace hardcoded base URL
+	const pathToCommentURL = `http://localhost:3000${paths.comments.comment.getHref(comment.id)}`;
 
 	return (
 		<div className={styles.comment}>
 			<div className={styles.meta}>
 				<span>{timeSinceCommentPosted}</span>
 				<span>
-					<Link size={16} />
+					<CopyButton textToCopy={pathToCommentURL} />
 				</span>
 			</div>
 			<p className={styles.content}>{comment.content}</p>
