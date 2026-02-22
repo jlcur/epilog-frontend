99fa00bdd3edaf21e34ae94168428c8e2b236927
jlcur <jordan@jordancurley.com>
2026-02-22 10:40:10 +0000

    Added toast notification on copy to clipboard

--- src/components/ui/copy-button/copy-button.tsx
+++ src/components/ui/copy-button/copy-button.tsx
@@ -1,5 +1,6 @@
 import { Check, Link } from "lucide-react";
 import type * as React from "react";
+import { toastManager } from "@/components/ui/toasts/toast-manager";
 import { useClipboard } from "@/hooks/use-clipboard";
 
 type CopyButtonProps = {
@@ -10,8 +11,15 @@ type CopyButtonProps = {
 export const CopyButton = ({ textToCopy, children }: CopyButtonProps) => {
 	const clipboard = useClipboard();
 
+	function copyToClipboard(textToCopy: string) {
+		clipboard.copy(textToCopy);
+		toastManager.add({
+			description: "Link copied to clipboard",
+		});
+	}
+
 	return (
-		<button type="button" onClick={() => clipboard.copy(textToCopy)}>
+		<button type="button" onClick={() => copyToClipboard(textToCopy)}>
 			{children}
 			{clipboard.state === "READY" && <Link size="14" />}
 			{clipboard.state === "SUCCESS" && (
