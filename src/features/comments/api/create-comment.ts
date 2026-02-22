ab3d6a0d93fad317da17a0889c6b66307e69763b
jlcur <jordan@jordancurley.com>
2026-02-22 14:51:51 +0000

    Implement basic comment creation form with formData

---
+++ src/features/comments/api/create-comment.ts
@@ -0,0 +1,39 @@
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+import { z } from "zod";
+import { endpoints } from "@/config/endpoints";
+import { queryKey } from "@/config/query-key";
+import { api } from "@/lib/api-client";
+import type { MutationConfig } from "@/lib/react-query";
+
+export const createCommentSchema = z.object({
+	content: z.string().min(1, "Content is required"),
+});
+
+export type CreateCommentInput = z.infer<typeof createCommentSchema>;
+
+export const createComment = ({ data }: { data: CreateCommentInput }) => {
+	return api.post(endpoints.comments.create, data);
+};
+
+type UseCreateCommentOptions = {
+	mutationConfig?: MutationConfig<typeof createComment>;
+};
+
+export const useCreateComment = ({
+	mutationConfig,
+}: UseCreateCommentOptions) => {
+	const queryClient = useQueryClient();
+
+	const { onSuccess, ...restConfig } = mutationConfig || {};
+
+	return useMutation({
+		onSuccess: (...args) => {
+			queryClient.invalidateQueries({
+				queryKey: queryKey.comments.all,
+			});
+			onSuccess?.(...args);
+		},
+		...restConfig,
+		mutationFn: createComment,
+	});
+};
