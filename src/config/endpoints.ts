99f24cd22e11f7462cb67fa7ca599db3cc3ef6fe
jlcur <jordan@jordancurley.com>
2026-02-25 16:04:33 +0000

    Include update query key definition

--- src/config/endpoints.ts
+++ src/config/endpoints.ts
@@ -4,5 +4,6 @@ export const endpoints = {
 		getById: (commentId: string) => `comment/${commentId}`,
 		delete: (commentId: string) => `comment/${commentId}`,
 		create: "comment",
+		update: (commentId: string) => `comment/${commentId}`,
 	},
 };
