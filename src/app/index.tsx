f6bbbb921d80d04db8bfe1ae7e36aa5f9ea62cb0
jlcur <jordan@jordancurley.com>
2026-03-10 12:02:32 +0000

    Add Figtree variabel font

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,5 +1,7 @@
 import { AppProvider } from "@/app/provider";
 import { AppRouter } from "@/app/router";
+// @ts-expect-error
+import "@fontsource-variable/figtree";
 
 export const App = () => {
 	return (
