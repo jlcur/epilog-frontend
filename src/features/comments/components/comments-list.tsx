0ad772f39eae760e7dd9987c58a2f93d82d48054
jlcur <jordan@jordancurley.com>
2026-03-10 11:46:13 +0000

    Update comment list styling

--- src/features/comments/components/comments-list.tsx
+++ src/features/comments/components/comments-list.tsx
@@ -5,6 +5,7 @@ import {
 	type CommentWithReplies,
 } from "@/features/comments/utils/build-comment-tree";
 import { useComments } from "../api/get-comments.ts";
+import styles from "./comments-list.module.css";
 
 export const CommentsList = () => {
 	const { data: comments = [], isPending } = useComments();
@@ -20,8 +21,8 @@ export const CommentsList = () => {
 	}
 
 	return (
-		<div>
-			<ul>
+		<div className={styles["comments-list"]}>
+			<ul className={styles.comment}>
 				{commentTree.map((comment: CommentWithReplies) => (
 					<li key={comment.id}>
 						<CommentCard comment={comment} />
