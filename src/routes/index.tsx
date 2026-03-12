69277f3bb861547cb86049bfef1f9c0377d57233
jlcur <jordan@jordancurley.com>
2026-03-12 14:04:10 +0000

    Conditionally render form based on user being logged in

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -2,15 +2,18 @@ import { createFileRoute } from "@tanstack/react-router";
 import { ContentLayout } from "@/components/layouts/content-layout";
 import { CommentsList } from "@/features/comments/components/comments-list";
 import {CreateComment} from "@/features/comments/components/create-comment";
+import {useSession} from "@/lib/auth/session";
 
 export const Route = createFileRoute("/")({
 	component: RouteComponent,
 });
 
 function RouteComponent() {
+	const { data: session } = useSession();
+
 	return (
 		<ContentLayout title="Epilog">
-			<CreateComment />
+			{session?.user ? <CreateComment parent={null} /> : <div>Log in or sign up to add comments</div> }
 			<CommentsList />
 		</ContentLayout>
 	);
