564fae8775cd539e5a5e8e78fd9820da59db2a3d
jlcur <jordan@jordancurley.com>
2026-03-25 13:47:06 +0000

    Refactor hardcoded API URL to use env variables

--- src/lib/auth-client.ts
+++ src/lib/auth-client.ts
@@ -1,7 +1,11 @@
 import { adminClient } from "better-auth/client/plugins";
 import { createAuthClient } from "better-auth/react";
 
+const authBaseUrl = import.meta.env.VITE_APP_API_URL
+	? `${import.meta.env.VITE_APP_API_URL}/auth`
+	: "http://localhost:3001/api/v1/auth";
+
 export const authClient = createAuthClient({
-	baseURL: "http://localhost:3001/api/v1/auth",
+	baseURL: authBaseUrl,
 	plugins: [adminClient()],
 });
